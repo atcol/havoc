fb05dd997fa902a92c35ab8c22f28bf44a55f88d
Alex Collins <alex@alexcollins.email>
2019-07-25 15:07:02 +0000

    Add imports to test pipeline

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Main where
 
-import           Network.Havoc (mkProxies)
+import qualified Data.ByteString   as BS
+import           Data.Text         (Text)
+import           Data.Yaml         (FromJSON (..), (.:))
+import qualified Data.Yaml         as Y
+import           Network.Havoc     (mkProxies)
+import           Text.RawString.QQ
 
 main :: IO ()
 main = head $ mkProxies [("https://google.com/", 80, 8080)]
